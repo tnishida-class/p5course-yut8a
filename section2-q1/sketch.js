00b585e29f3f2416571f65772dfb6f96d45176f1
yut8a <92322168+yut8a@users.noreply.github.com>
2021-10-20 00:05:33 +0900

    section2-q1
    add
    function setup() {
      noFill();
    
      for(let i = 0; i < 10; i++){
        let r = i * 10 + 10;
        if(i < 5){// blue
          stroke(0,0,255);
        }
        else{// red
          stroke(255,0,0);
        }
        ellipse(50,50,r);
        }
      }

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
-// 小手調べ
-function setup(){
+function setup() {
+  noFill();
+
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let r = i * 10 + 10;
+    if(i < 5){// blue
+      stroke(0,0,255);
+    }
+    else{// red
+      stroke(255,0,0);
+    }
+    ellipse(50,50,r);
+    }
   }
-}
