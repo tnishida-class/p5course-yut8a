79913cd9b5feb33bd406190c463deb2355367f53
yut8a <92322168+yut8a@users.noreply.github.com>
2021-10-20 03:21:30 +0900

    section-2-q2
    add
       if (i % 2 == 0){
            if(j % 2 == 1){
              fill("grey");
              rect(size * i, size * j, size, size);
              if (j<3) {
                fill("red");
                ellipse(size*i+size/2, size*j+size/2,size);
                arc(size*i+size/2,size*j+size/2,size,PI*2);
               }
               if (j>4) {
                 fill("black");
                 ellipse(size*i+size/2, size*j+size/2,size);
                 arc(size*i+size/2,size*j+size/2,size,PI*2);
                }
    
            }
          }
          if (i % 2 == 1){
            if(j % 2 == 0){
              fill("grey");
              rect(size * i, size * j, size, size);
              if (j<3) {
                fill("red");
                ellipse(size*i+size/2, size*j+size/2,size);
                arc(size*i+size/2,size*j+size/2,size,PI*2);
              }
              if (j>4) {
                fill("black");
                ellipse(size*i+size/2, size*j+size/2,size);
                arc(size*i+size/2,size*j+size/2,size,PI*2);
               }
            }
          }

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,39 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if (i % 2 == 0){
+        if(j % 2 == 1){
+          fill("grey");
+          rect(size * i, size * j, size, size);
+          if (j<3) {
+            fill("red");
+            ellipse(size*i+size/2, size*j+size/2,size);
+            arc(size*i+size/2,size*j+size/2,size,PI*2);
+           }
+           if (j>4) {
+             fill("black");
+             ellipse(size*i+size/2, size*j+size/2,size);
+             arc(size*i+size/2,size*j+size/2,size,PI*2);
+            }
+
+        }
+      }
+      if (i % 2 == 1){
+        if(j % 2 == 0){
+          fill("grey");
+          rect(size * i, size * j, size, size);
+          if (j<3) {
+            fill("red");
+            ellipse(size*i+size/2, size*j+size/2,size);
+            arc(size*i+size/2,size*j+size/2,size,PI*2);
+          }
+          if (j>4) {
+            fill("black");
+            ellipse(size*i+size/2, size*j+size/2,size);
+            arc(size*i+size/2,size*j+size/2,size,PI*2);
+           }
+        }
+      }
     }
   }
 }
