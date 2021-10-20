efd2c87a9e78be582f9fa781291f564e07ad6aeb
yut8a <92322168+yut8a@users.noreply.github.com>
2021-10-20 11:25:26 +0900

    section2-q2
    add
    コメントを増やした

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,20 +1,21 @@
-// チェッカー
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       if (i % 2 == 0){
         if(j % 2 == 1){
+          //グレーで四角を塗る
           fill("grey");
           rect(size * i, size * j, size, size);
           if (j<3) {
+            //赤の円を描く
             fill("red");
             ellipse(size*i+size/2, size*j+size/2,size);
             arc(size*i+size/2,size*j+size/2,size,PI*2);
            }
            if (j>4) {
+             //黒の円を描く
              fill("black");
              ellipse(size*i+size/2, size*j+size/2,size);
              arc(size*i+size/2,size*j+size/2,size,PI*2);
@@ -24,14 +25,17 @@ function setup() {
       }
       if (i % 2 == 1){
         if(j % 2 == 0){
+          //グレーで四角を塗る
           fill("grey");
           rect(size * i, size * j, size, size);
           if (j<3) {
+            //赤の円を描く
             fill("red");
             ellipse(size*i+size/2, size*j+size/2,size);
             arc(size*i+size/2,size*j+size/2,size,PI*2);
           }
           if (j>4) {
+            //黒の円を描く
             fill("black");
             ellipse(size*i+size/2, size*j+size/2,size);
             arc(size*i+size/2,size*j+size/2,size,PI*2);
