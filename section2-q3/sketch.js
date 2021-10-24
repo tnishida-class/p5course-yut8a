f9fd3157977243a15d31c8bc69d421848c65ddbf
yut8a <92322168+yut8a@users.noreply.github.com>
2021-10-25 00:18:43 +0900

    section2-q3
    add
    +  drawArcs(cream, black, maxR * 0.75);
      drawArcs(green, red, maxR * 0.6);
      drawArcs(cream, black, maxR * 0.55);
      drawCircle(green, maxR * 0.15);
      drawCircle(red, maxR * 0.08);

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -16,16 +16,24 @@ function setup() {
 
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
-  // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
-  drawCircle(red, maxR * 0.05);
+  drawArcs(cream, black, maxR * 0.75);
+  drawArcs(green, red, maxR * 0.6);
+  drawArcs(cream, black, maxR * 0.55);
+  drawCircle(green, maxR * 0.15);
+  drawCircle(red, maxR * 0.08);
 }
 
 function drawCircle(c, r){
+  const cx = width / 2;
+  const cy = height / 2;
   fill(c);
   ellipse(cx, cy, r, r);
 }
 
 function drawArcs(c1, c2, r) {
+  const cx = width / 2;
+  const cy = height / 2;
+
   for (let i = 0; i < 20; i++) {
     let start = TWO_PI / 20 * i;
     let stop = TWO_PI / 20 * (i + 1);
