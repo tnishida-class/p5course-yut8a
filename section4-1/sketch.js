b3845e2ea3ff675a8541471571cdcb28ac624db5
yut8a <92322168+yut8a@users.noreply.github.com>
2021-11-08 09:19:28 +0900

    Update sketch.js

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -19,32 +19,54 @@ function setup(){
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
+  average = sum / scores.length;
+  console.log(average);
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+    if(largest < scores[i]){
+      largest = scores[i];
+    }
   }
+  console.log(largest);
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[3]　ヒント：最小値とだいたい同じ
+    if(smallest > scores[i]){
+      smallest = scores[i];
+    }
   }
+  console.log(smallest);
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
+
   noStroke();
 
   for(let i = 0; i < scores.length; i++){
-    const dx = width / scores.length;
+    const dx = width / scores.length ;
     const h = height * scores[i] / 100;
-    // BLANK[4] ヒント: 条件分岐を使って色を変更します
-    rect(i * dx + 2, height - h, dx - 4, h);
+    if(scores[i] == smallest){
+    fill(0,0,255);
+    }
+    else if (scores[i] == largest){
+    fill(204,0,0);
+    }
+    else{
     fill(0);
+    }
+    rect(i * dx + 2, height - h, dx - 4, h);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
-
-  // BLANK[5] 平均点の線を引きます
+  stroke(0,255,0);
+  line(0,height - average * height / 100 , width ,
+    height- average * height / 100);
+  stroke(204,0,0)
+  line(0,height - largest * height / 100 , width
+    , height- largest * height / 100);
+  stroke(0,0,255)
+  line(0,height - smallest * height / 100 , width
+    , height- smallest * height / 100);
 }
