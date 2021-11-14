112645093e8470fe538cc2dc057466867a5e2dcc
yut8a <92322168+yut8a@users.noreply.github.com>
2021-11-14 16:57:07 +0900

    section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon(200,200,"I love hinatazaka46",255,255,0);
 }
 
-function balloon(t){
+function balloon(x,y,t,r,g,b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(r,g,b);
+  text(t, x, h + x);
+  fill(0);
+  triangle(x, y+(6/5*h), x*(19/20), y+2*h, x+x/3, y+h)
 }
